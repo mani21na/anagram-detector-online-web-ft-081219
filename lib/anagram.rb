24d7a2bf47989f8b951fb648d1437164a53bdec3
Hanna Lee <mani21na@gmail.com>
2019-08-23 22:34:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def desclamble(word)
+    word.split("").sort
+  end
+  
+  def match(array_word)
+    array = []
+    array_word.each do |word|
+      if desclamble(word) == desclamble(@word)
+        array << word
+      end
+ #     binding.pry
+    end
+    array  
+  end
+end
+
+
+test= Anagram.new("test")
+test.match(%w(enlists google inlets banana estt))
