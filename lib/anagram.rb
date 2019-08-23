337445c224bbfc8f5bdf568265c51230c5c79e46
Hanna Lee <mani21na@gmail.com>
2019-08-23 22:34:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,16 @@
 # Your code goes here!
 require 'pry'
 
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+end
+
+
 =begin
 class Anagram
   attr_accessor :word
